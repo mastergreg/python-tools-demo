a52f873b77bee13465853a64393d6c74dc9eba5b
Greg Lyras <greglyras@gmail.com>
2021-12-08 23:35:03 +0000

    this passed the checks

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+#
+# * -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+# File Name : main.py
+# Creation Date : 08-12-2021
+# Last Modified : Wed 08 Dec 2021 23:34:51 GMT
+# Created By : Greg Lyras <greglyras@gmail.com>
+# _._._._._._._._._._._._._._._._._._._._._.*/
+
+
+def main():
+    my_id = 0
+    print(my_id)
+
+
+if __name__ == "__main__":
+    main()
